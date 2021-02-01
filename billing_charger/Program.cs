7626fd136dcb9aeac876d057b5250fa044ab5a7a
Daniel Long <danlong5@gmail.com>
2021-01-31 21:43:17 -0600

    removing the consumer and adding the charger

---
+++ billing_charger/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace billing_charger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumerConfig config = new ConsumerConfig
+            {   
+                GroupId = "billing_charger",
+                BootstrapServers = "kafka:9092"
+            };
+            
+            IConsumer<Ignore, string> consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("billing");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; 
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        ConsumeResult<Ignore, string> consumeResult = consumer.Consume(cts.Token);
+                        Console.WriteLine($"Message Received: {consumeResult.Message.Value}");
+                    }
+                    catch (ConsumeException ex)
+                    {
+                        Console.WriteLine($"Error Found: {ex.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException exception)
+            {
+                Console.WriteLine($"Error Found: {exception.Message}");
+                consumer.Close();
+            }
+        }
+    }
+}
+    
