42365aad5cdf4f06f050ee70c89b169acc023ef3
Daniel Long <danlong5@gmail.com>
2021-02-13 22:40:47 -0600

    first draft of callbacks

---
+++ billing_callbacks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace billing_callbacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"command: {args[0]}");
+            Console.WriteLine($"job name: {args[1]}");
+            Console.WriteLine($"job count: {args[2]}");
+            
+            // start the correct background job based on args[0]
+            
+        }
+    }
+}
