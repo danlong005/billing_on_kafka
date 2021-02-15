90e7968f1da94a55676d38132e414b0ef9a551e9
Daniel Long <danlong5@gmail.com>
2021-02-14 22:05:57 -0600

    adding the callbacks

---
+++ billing_callbacks/simple_decline_callback/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simple_decline_callback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
